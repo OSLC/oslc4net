f5f60950e22a931897f1a2754bc05f080a8034a8
Michael Fiedler <fiedler.mf@gmail.com>
2013-02-12 13:41:53 -0500

    Update copyrights

--- OSLC4Net_SDK/OSLC4Net.StockQuoteSample/Models/Utilities.cs
+++ OSLC4Net_SDK/OSLC4Net.StockQuoteSample/Models/Utilities.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *  
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Michael Fiedler  - initial API and implementation
+ *******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
