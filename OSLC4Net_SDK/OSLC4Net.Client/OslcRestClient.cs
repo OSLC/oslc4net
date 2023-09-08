c9d8788c028906b222abaf78564d209ace5c3d2e
Andrew Berezovskyi <andriib@kth.se>
2023-09-08 19:05:46 +0200

    feat: target netstd2.0 in CM test and refactor Client.Jazz out of Client

--- OSLC4Net_SDK/OSLC4Net.Client/OslcRestClient.cs
+++ OSLC4Net_SDK/OSLC4Net.Client/OslcRestClient.cs
@@ -15,13 +15,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 using OSLC4Net.Core.Model;
 
