7b0ec917d3a1be64814a229b3ea8c25f1ef285b2
Andrew Berezovskyi <andrew@berezovskyi.me>
2024-09-14 14:21:23 +0200

    update OslcResponse interfaces
    switch to providing RestSharp-specific data through a mixin pattern

--- OSLC4Net_SDK/OSLC4Net.Client.RestSharp/IOslcResponse.cs
+++ OSLC4Net_SDK/OSLC4Net.Client.RestSharp/IOslcResponse.cs
@@ -3,8 +3,41 @@ using OSLC4Net.Core.Model;
 
 namespace OSLC4Net.Client.RestSharp;
 
+/// <summary>
+///     Interface for responses for OSLC resource requests.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IOslcResponse<T> where T : IResource
 {
     Uri Uri { get; }
+
+    OslcResponseStatus Status { get; }
+}
+
+public enum OslcResponseStatus
+{
+    Success,
+    SuccessNoResource,
+    AuthnFault,
+    ClientNonAuthnFault,
+    ServerFault
+}
+
+/// <summary>
+///     Interface for a successful OSLC resource request containing a resource in the response.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IOslcResourceResponse<T> : IOslcResponse<T> where T : IResource
+{
     T Resource { get; }
+}
+
+
+/// <summary>
+///     Interface for a successful OSLC resource request containing a resource in the response.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IOslcAuthNeededResponse<T> : IOslcResponse<T> where T : IResource
+{
+    
 }
