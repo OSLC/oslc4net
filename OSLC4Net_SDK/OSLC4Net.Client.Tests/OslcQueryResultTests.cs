9f4a3a2a6d29a00b488b6f3eff95c1cc3ad52469
Andrew Berezovskyi <andriib@kth.se>
2024-10-05 12:00:22 +0200

    test: prep the project

---
+++ OSLC4Net_SDK/OSLC4Net.Client.Tests/OslcQueryResultTests.cs
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Andrii Berezovskyi and oslc4net contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ *******************************************************************************/
+
+using System.Diagnostics;
+using OSLC4Net.Core.Query;
+using Xunit;
+using ParseException = OSLC4Net.Core.Query.ParseException;
+
+namespace OSLC4Net.Client.Oslc.Resources;
+
+public class OslcQueryResultTests
+{
+
+    [Fact]
+    public void OslcQueryMultiResponseTest()
+    {
+        Assert.True(false);
+    }
+}
---
+++ OSLC4Net_SDK/OSLC4Net.Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0825705-7006-4440-a48f-7d970b546fa3")]
