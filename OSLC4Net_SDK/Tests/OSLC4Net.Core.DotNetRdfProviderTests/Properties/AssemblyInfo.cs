fda175dd29b7528954ec249301bec0bf53f6fbbf
Andrew Berezovskyi <andriib@kth.se>
2024-12-07 00:49:23 +0100

    fix: remove conflicting assembly attrs

--- OSLC4Net_SDK/Tests/OSLC4Net.Core.DotNetRdfProviderTests/Properties/AssemblyInfo.cs
+++ OSLC4Net_SDK/Tests/OSLC4Net.Core.DotNetRdfProviderTests/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
