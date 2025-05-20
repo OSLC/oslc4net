56b8a15a5081e661414bcc9aa62e9a11c8bf5f28
Andrew Berezovskyi <andriib@kth.se>
2025-05-20 23:52:19 +0200

    fix: aspire tests

--- OSLC4Net_SDK/Tests/OSLC4Net.Test.AspireHost/Program.cs
+++ OSLC4Net_SDK/Tests/OSLC4Net.Test.AspireHost/Program.cs
@@ -1,7 +1,18 @@
+using System.Net;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-// NOTE: we need to grab the endpoint, thus this container is added in the DistributedApplicationTestingBuilder
-//builder.AddContainer("refimpl-cm", "server-cm")
-//    .WithEndpoint(port: 8801, targetPort: 8080, scheme: "http", name: "http");
+var refimplCM = builder
+    .AddDockerfile("refimpl-cm", "../../../../refimpl/src/", "server-cm/Dockerfile")
+    .WithEndpoint(8801, 8080, isExternal: true, isProxied: false,
+        scheme: "http", name: "http")
+    .WithHttpHealthCheck("/services/catalog/singleton", (int)HttpStatusCode.Unauthorized);
 
-builder.Build().Run();
+var refimplRM = builder
+    .AddDockerfile("refimpl-rm", "../../../../refimpl/src/", "server-rm/Dockerfile")
+    .WithEndpoint(8800, 8080, isExternal: true, isProxied: false,
+        scheme: "http", name: "http")
+    .WithHttpHealthCheck("/services/catalog/singleton", (int)HttpStatusCode.Unauthorized);
+
+
+await builder.Build().RunAsync();
--- OSLC4Net_SDK/Tests/OSLC4Net.Test.RefImpl/RefimplAspireFixture.cs
+++ OSLC4Net_SDK/Tests/OSLC4Net.Test.RefImpl/RefimplAspireFixture.cs
@@ -1,4 +1,3 @@
-using System.Net;
 using Aspire.Hosting;
 using Aspire.Hosting.Testing;
 using OSLC4Net.ChangeManagementTest;
@@ -14,15 +13,11 @@ namespace OSLC4Net.ChangeManagementTest;
 
 public class RefimplAspireFixture : IAsyncLifetime
 {
-    private const int TimeoutHealthcheck = 1; // seconds
-
-    // between retries, seconds
-    private const int DelayHealthcheck = 1;
-
     public async ValueTask DisposeAsync()
     {
         if (DistributedApplication is not null)
         {
+            await DistributedApplication.StopAsync();
             await DistributedApplication.DisposeAsync();
         }
     }
@@ -39,17 +34,6 @@ public class RefimplAspireFixture : IAsyncLifetime
         var builder = await DistributedApplicationTestingBuilder
             .CreateAsync<OSLC4Net_Test_AspireHost>().ConfigureAwait(true);
 
-        var refimplCM = builder
-            .AddDockerfile("refimpl-cm", "../../../../refimpl/src/", "server-cm/Dockerfile")
-            .WithEndpoint(8801, 8080, isExternal: true, isProxied: false,
-                scheme: "http", name: "http");
-
-        var refimplRM = builder
-            .AddDockerfile("refimpl-rm", "../../../../refimpl/src/", "server-rm/Dockerfile")
-            .WithEndpoint(8800, 8080, isExternal: true, isProxied: false,
-                scheme: "http", name: "http");
-
-
         //builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
         //{
         //    clientBuilder.AddStandardResilienceHandler();
@@ -58,61 +42,26 @@ public class RefimplAspireFixture : IAsyncLifetime
         // To output logs to the xUnit.net ITestOutputHelper,
         // consider adding a package from https://www.nuget.org/packages?q=xunit+logging
 
-        await using var app = await builder.BuildAsync().ConfigureAwait(true);
+        // builder.
+        var app = await builder.BuildAsync();
 
-        await app.StartAsync().ConfigureAwait(true);
+        await app.StartAsync().WaitAsync(TimeSpan.FromSeconds(300)).ConfigureAwait(true);
 
+        await app.ResourceNotifications.WaitForResourceHealthyAsync("refimpl-cm")
+            .WaitAsync(TimeSpan.FromSeconds(300)).ConfigureAwait(true);
+        await app.ResourceNotifications.WaitForResourceHealthyAsync("refimpl-rm")
+            .WaitAsync(TimeSpan.FromSeconds(300)).ConfigureAwait(true);
+
+        var endpoint = app.GetEndpoint("refimpl-cm", "http");
         ServiceProviderCatalogUriCM =
-            refimplCM.GetEndpoint("http").Url + "/services/catalog/singleton";
-        // ServiceProviderCatalogUriRM = "http://localhost:8800/services/catalog/singleton";
+            endpoint.AbsoluteUri + "services/catalog/singleton";
         ServiceProviderCatalogUriRM =
-            refimplRM.GetEndpoint("http").Url + "/services/catalog/singleton";
-        // Poll for server availability
-        await AwaitRefimplStartupAsync().ConfigureAwait(true);
+            app.GetEndpoint("refimpl-rm", "http").AbsoluteUri + "services/catalog/singleton";
 
         return app;
     }
 
-    public string ServiceProviderCatalogUriRM { get; set; }
+    public string ServiceProviderCatalogUriRM { get; private set; }
 
-    public string ServiceProviderCatalogUriCM { get; set; }
-
-    private async Task AwaitRefimplStartupAsync()
-    {
-        var httpClient = new HttpClient();
-        httpClient.Timeout = TimeSpan.FromSeconds(TimeoutHealthcheck);
-        var maxRetries = 180;
-        var retryDelay = TimeSpan.FromSeconds(DelayHealthcheck);
-        var serverUrlCm = new Uri(ServiceProviderCatalogUriCM);
-        var serverUrlRm = new Uri(ServiceProviderCatalogUriRM);
-
-        for (var i = 0; i < maxRetries; i++)
-        {
-            try
-            {
-                var response = await httpClient.GetAsync(serverUrlCm).ConfigureAwait(true);
-                var responseRm = await httpClient.GetAsync(serverUrlRm).ConfigureAwait(true);
-                if (response.StatusCode == HttpStatusCode.Unauthorized &&
-                    responseRm.StatusCode == HttpStatusCode.Unauthorized)
-                {
-                    // Server is ready
-                    break;
-                }
-            }
-            catch
-            {
-                // Server not ready yet
-            }
-
-            if (i < maxRetries - 1)
-            {
-                await Task.Delay(retryDelay).ConfigureAwait(true);
-            }
-            else
-            {
-                throw new TimeoutException(
-                    $"Server at {serverUrlCm} or {serverUrlRm} did not start within {maxRetries * retryDelay.TotalSeconds} seconds");
-            }
-        }
-    }
+    public string ServiceProviderCatalogUriCM { get; private set; }
 }
