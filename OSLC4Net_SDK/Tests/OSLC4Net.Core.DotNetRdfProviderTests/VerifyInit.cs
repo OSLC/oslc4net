3395ce32028d2faf842c035eb94a5c21eaef963d
Andrew Berezovskyi <andriib@kth.se>
2025-11-22 16:29:07 +0100

    refactor: verify init
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- OSLC4Net_SDK/Tests/OSLC4Net.Core.DotNetRdfProviderTests/VerifyInit.cs
+++ OSLC4Net_SDK/Tests/OSLC4Net.Core.DotNetRdfProviderTests/VerifyInit.cs
@@ -3,6 +3,7 @@ using EmptyFiles;
 using OSLC4Net.Core.DotNetRdfProvider;
 using OSLC4Net.Core.Model;
 using VDS.RDF;
+using VDS.RDF.Writing;
 
 namespace OSLC4Net.Core.DotNetRdfProviderTests;
 
@@ -20,44 +21,34 @@ public static class VerifyInit
 
         FileExtensions.AddTextExtension("ttl");
 
-        VerifierSettings.RegisterFileConverter<IGraph>((graph, context) =>
-        {
-            using var writer = new StringWriter();
-
-            var rdfc = new RdfCanonicalizer();
-            var graphCollection = new GraphCollection
+        VerifierSettings.RegisterFileConverter<IGraph>(
+            (graph, _) =>
             {
-                { graph, false }
-            };
-            var canonicalizedRdfDataset = rdfc.Canonicalize(new TripleStore(graphCollection));
-
-            var newGraph = canonicalizedRdfDataset.OutputDataset.Graphs.Single();
-
-            // https://www.w3.org/TR/rdf-canon/ prescribes N-Quads serialization, but let's try Turtle for better readability
-            var ttlWriter = new VDS.RDF.Writing.CompressingTurtleWriter();
-            ttlWriter.Save(newGraph, writer);
-            return new(null, "ttl", writer.ToString());
-        });
-
-        VerifierSettings.RegisterFileConverter<IResource>((resource, context) =>
-        {
-            using var writer = new StringWriter();
-            var graph = DotNetRdfHelper.CreateDotNetRdfGraph([resource]);
-
-            var rdfc = new RdfCanonicalizer();
-            var graphCollection = new GraphCollection
+                return CanonicalizeAndSerializeGraph(graph);
+            });
+        VerifierSettings.RegisterFileConverter<IResource>(
+            (resource, _) =>
             {
-                { graph, false }
-            };
-            var canonicalizedRdfDataset = rdfc.Canonicalize(new TripleStore(graphCollection));
-
-            var newGraph = canonicalizedRdfDataset.OutputDataset.Graphs.Single();
-
-            // https://www.w3.org/TR/rdf-canon/ prescribes N-Quads serialization, but let's try Turtle for better readability
-            var ttlWriter = new VDS.RDF.Writing.CompressingTurtleWriter();
-            // ttlWriter.Save(graph, writer);
-            ttlWriter.Save(newGraph, writer);
-            return new(null, "ttl", writer.ToString());
-        });
+                var graph = DotNetRdfHelper.CreateDotNetRdfGraph([resource]);
+                return CanonicalizeAndSerializeGraph(graph);
+            });
     }
+
+    private static ConversionResult CanonicalizeAndSerializeGraph(IGraph graph)
+    {
+        using var writer = new System.IO.StringWriter();
+        var rdfc = new RdfCanonicalizer();
+        using var graphCollection = new GraphCollection
+        {
+            { graph, false }
+        };
+        using var tripleStore = new TripleStore(graphCollection);
+        var canonicalizedRdfDataset = rdfc.Canonicalize(tripleStore);
+        var newGraph = canonicalizedRdfDataset.OutputDataset.Graphs.Single();
+        // https://www.w3.org/TR/rdf-canon/ prescribes N-Quads serialization, but let's try Turtle for better readability
+        var ttlWriter = new CompressingTurtleWriter();
+        ttlWriter.Save(newGraph, writer);
+        return new ConversionResult(null, "ttl", writer.ToString());
+    }
+
 }
