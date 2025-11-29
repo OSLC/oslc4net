591f92905e74cd60ffc57f6851b8958c951327fd
Andrew Berezovskyi <andriib@kth.se>
2025-11-29 20:08:06 +0100

    fix: nullability
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- OSLC4Net_SDK/OSLC4Net.Client/Oslc/Resources/RmUtil.cs
+++ OSLC4Net_SDK/OSLC4Net.Client/Oslc/Resources/RmUtil.cs
@@ -21,49 +21,63 @@ namespace OSLC4Net.Client.Oslc.Resources;
 
 public static class RmUtil
 {
-    public static async Task<ResourceShape> LookupRequirementsInstanceShapesAsync(string serviceProviderUrl,
+    public static async Task<ResourceShape?> LookupRequirementsInstanceShapesAsync(
+        string serviceProviderUrl,
         string oslcDomain, string oslcResourceType, OslcClient client, string requiredInstanceShape)
     {
         var response = await client.GetResourceAsync<ServiceProvider>(serviceProviderUrl).ConfigureAwait(false);
         //var formatters = client.GetFormatters();
-        var serviceProvider = response.Resources.SingleOrDefault();
+        var serviceProvider = response.Resources?.SingleOrDefault();
 
-        if (serviceProvider != null)
+        if (serviceProvider == null)
         {
-            foreach (var service in serviceProvider.GetServices())
+            throw new ResourceNotFoundException(serviceProviderUrl, "InstanceShapes");
+        }
+
+        foreach (var service in serviceProvider.GetServices())
+        {
+            var domain = service.GetDomain();
+            if (domain == null || !domain.ToString().Equals(oslcDomain))
             {
-                var domain = service.GetDomain();
-                if (domain != null && domain.ToString().Equals(oslcDomain))
+                continue;
+            }
+
+            var creationFactories = service.GetCreationFactories();
+            if (creationFactories == null || creationFactories.Length <= 0)
+            {
+                continue;
+            }
+
+            foreach (var creationFactory in creationFactories)
+            {
+                foreach (var resourceType in creationFactory.GetResourceTypes())
                 {
-                    var creationFactories = service.GetCreationFactories();
-                    if (creationFactories != null && creationFactories.Length > 0)
+                    if (resourceType.ToString() == null ||
+                        !resourceType.ToString().Equals(oslcResourceType))
                     {
-                        foreach (var creationFactory in creationFactories)
+                        continue;
+                    }
+
+                    var instanceShapes = creationFactory.GetResourceShapes();
+                    if (instanceShapes == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (var typeURI in instanceShapes)
+                    {
+                        var typeResponse = await client.GetResourceAsync<ResourceShape>(typeURI)
+                            .ConfigureAwait(false);
+                        var resourceShape =
+                            typeResponse.Resources?.SingleOrDefault();
+                        var typeTitle = resourceShape?.GetTitle();
+                        if (typeTitle == null || string.Compare(typeTitle,
+                                requiredInstanceShape, true) != 0)
                         {
-                            foreach (var resourceType in creationFactory.GetResourceTypes())
-                            {
-                                if (resourceType.ToString() != null &&
-                                    resourceType.ToString().Equals(oslcResourceType))
-                                {
-                                    var instanceShapes = creationFactory.GetResourceShapes();
-                                    if (instanceShapes != null)
-                                    {
-                                        foreach (var typeURI in instanceShapes)
-                                        {
-                                            var typeResponse = await client.GetResourceAsync<ResourceShape>(typeURI)
-                                                .ConfigureAwait(false);
-                                            var resourceShape = typeResponse.Resources.SingleOrDefault();
-                                            var typeTitle = resourceShape.GetTitle();
-                                            if (typeTitle != null && string.Compare(typeTitle,
-                                                    requiredInstanceShape, true) == 0)
-                                            {
-                                                return resourceShape;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            continue;
                         }
+
+                        return resourceShape;
                     }
                 }
             }
